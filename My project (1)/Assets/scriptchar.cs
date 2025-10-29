c9aabeeafc1e97bc79ca127bf129adfa35e011d4
abrar247724 <abrar247724@bue.edu.eg>
2025-10-29 04:59:58 -0700

    Lab5

--- My project (1)/Assets/scriptchar.cs
+++ My project (1)/Assets/scriptchar.cs
@@ -14,8 +14,12 @@ public Transform groundCheck;
 public float groundCheckRadius;
 public LayerMask whatIsGround;
 private bool grounded;
+private Animator anim;
+public Transform Target;
+
 
 void Start(){
+anim = GetComponent<Animator>();
 
 }
 
@@ -43,7 +47,11 @@ if(GetComponent<SpriteRenderer>() != null){
 
 GetComponent<SpriteRenderer>().flipX = false;
 }
-}}
+}
+anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+anim.SetBool("Grounded", grounded);
+}
 void Jump(){
 
 GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
